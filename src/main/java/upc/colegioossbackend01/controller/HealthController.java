f48df956abb86c954b8eb18b5eb3e09b7824d19f
EricsonCR <ericson4634@gmail.com>
2026-07-08 04:33:32 -0500

    add controller health get(/)

---
+++ src/main/java/upc/colegioossbackend01/controller/HealthController.java
@@ -0,0 +1,16 @@
+package upc.colegioossbackend01.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HealthController {
+
+    @GetMapping
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Colegio OSS Backend funcionando");
+    }
+}
